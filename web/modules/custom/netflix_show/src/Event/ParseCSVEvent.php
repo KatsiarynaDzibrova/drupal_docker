fc7e91cad07509fc02af66a131bbdb7db6212b62
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2021-03-11 12:12:39 +0300

    D-8: add show image

--- web/modules/custom/netflix_show/src/Event/ParseCSVEvent.php
+++ web/modules/custom/netflix_show/src/Event/ParseCSVEvent.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\netflix_show\Event;
 
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\file\Entity\File;
 use Symfony\Component\EventDispatcher\Event;
+use GuzzleHttp\Client;
 
 /**
  * Event that is parses csv with shows and makes nodes.
@@ -34,24 +36,53 @@ class ParseCSVEvent extends Event {
       for ($i = 0; $i < 2; $i++) {
         if (($data = fgetcsv($handle)) === FALSE) break;
         if ($i == 0) continue;
+        $title = $data[2];
         $fields = [
           'type' => 'netflix_title',
-          'title' => $data[2],
+          'title' => $title,
           'field_text_plain' => $data[3],
           'field_text_plain_long' => $data[4],
           'field_text_plain_1' => $data[5],
           'field_number_int' => $data[7],
           'field_text_plain_2' => $data[9],
-          // TODO
-          // 'field_image' => ,
           'field_text_plain_long_1' => $data[11],
         ];
         $node = \Drupal::entityTypeManager()
           ->getStorage('node')
           ->create($fields);
-        $node->save();
+        if ($image_url = $this->getImage($title) != '') {
+          $image = file_get_contents($image_url);
+          $file_temp = system_retrieve_file($image, NULL, TRUE, FileSystemInterface::EXISTS_RENAME);
+          if ($file_temp !== FALSE) {
+            $node->set('field_image', ['target_id' => $file_temp->fid,]);
+          }
+          $node->save();
+        }
       }
     }
+  }
+
+  private function getImage($title) {
+    $client = new Client;
+
+    $request = $client->request('GET', 'https://movie-database-imdb-alternative.p.rapidapi.com/', [
+      'query' => [
+        's' => $title,
+        'page' => '1',
+        'r' => 'json',
+      ],
+      'headers' => [
+        'x-rapidapi-key' => 'c554ec35c5msh4eabb92fa586e9bp1e0957jsn68ea1eccfc74',
+        'x-rapidapi-host' => 'movie-database-imdb-alternative.p.rapidapi.com'
+      ]
+    ]);
+
+    $response = $request->getBody()->getContents();
+    $json = json_decode($response);
+    if ($json->Response) {
+      return $json->Search[0]->Poster;
+    }
+    return '';
   }
 }
 
