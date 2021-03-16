405b2832026b1447ce95a484c6ca026d53ff3621
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2021-03-16 11:21:14 +0300

    D-8: use dependency injection

--- web/modules/custom/netflix_show/src/Form/ModuleUploadForm.php
+++ web/modules/custom/netflix_show/src/Form/ModuleUploadForm.php
@@ -5,11 +5,28 @@ namespace Drupal\netflix_show\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\netflix_show\Event\ParseCSVEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a form that configures forms module settings.
  */
 class ModuleUploadForm extends FormBase {
+
+  protected $dispatcher;
+
+  public function __construct($dispatcher) {
+    $this->dispatcher = $dispatcher;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      $container->get('event_dispatcher')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -41,9 +58,8 @@ class ModuleUploadForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $file = $form_state->getValue('csvfile', 0);
-    $dispatcher = \Drupal::service('event_dispatcher');
     $e = new ParseCSVEvent($file);
-    $event = $dispatcher->dispatch(ParseCSVEvent::EVENT_NAME, $e);
+    $event = $this->dispatcher->dispatch(ParseCSVEvent::EVENT_NAME, $e);
     $event->run();
   }
 }
